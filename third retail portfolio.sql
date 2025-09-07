6face4893e10b716e9be4b713bc85e95d4d2aa93
sala123-peace <salmahmohd50@gmail.com>
2025-09-07 14:37:45 +0100

    Update third retail portfolio.sql

--- third retail portfolio.sql
+++ third retail portfolio.sql
@@ -12,19 +12,23 @@ create table Retail_sale(
        cogs float,
         retail_saletotal_sale float);
         
-              
         select *from retail_sale;
         where 
         transaction_id is null
         or sale_date is null
         or sale_time is null
         or gender is  null; 
--- write query to see all the transcations on 2022-11-05	
+
+
+1.-- write query to see all the transcations on 2022-11-05	
+	***sql
 select *
 from retail_sale
 where sale_date = '2022-11-05';
+***
 
--- write a query to see retrive all the category where transactions is clothing and the quantity sold is more than 10 in nov 2022
+2.-- write a query to see retrive all the category where transactions is clothing and the quantity sold is more than 10 in nov 2022
+	***sql
 select category,sum(quantity)as total_sale
 from retail_sale
 where category = 'clothing'
@@ -32,59 +36,74 @@ and
 date_format(sale_date,'%y-%m') = '2022-11'
 and
 quantity = 3;
+***
 
--- write a query to calc total_sale(total_sale) for each category
+3.-- write a query to calc total_sale(total_sale) for each category
+	***sql
 select category,
 SUM(total_sale) as total,
 count(*) as total_foreach
 from retail_sale
 group by 1;
-
--- write a query to find the average age of customers who purchased from the beauty category
+***
+	
+4.-- write a query to find the average age of customers who purchased from the beauty category
+	***sql
 select
 avg(age)
 from retail_sale
 where category ='beauty';
+***
 
--- write a query to find all transactions where total_sale  is > 1000
+5.-- write a query to find all transactions where total_sale  is > 1000
+	***sql
     select 
     total_sale as total
     from retail_sale
         where total_sale > 1000;
+***
 
-    -- write a query to find the total number of transactions(transaction_id) made by each gender in each category
-    
+  6.  -- write a query to find the total number of transactions(transaction_id) made by each gender in each category
+    ***sql
     select category,
     gender,
     count(*) as total_trans
     from retail_sale
     group by category,gender
     order by 1;
+***
     
--- write a query to calc the average sale for each month and find the best selling month in each year
+7.-- write a query to calc the average sale for each month and find the best selling month in each year
+	***sql
 select 
 year(sale_date),
 month(sale_date),
 sum(total_sale) as total_sale
 from retail_sale
 group by 1,2;
+***
 
--- write a query to see the number of unique customers who purchased items in each category
-    
+8.-- write a query to see the number of unique customers who purchased items in each category
+    ***sql
     select category,
     sum(distinct customer_id) as uniquecustomer
     from retail_sale
     group by category;
+***
     
-    -- write query to see top 5 customers based on total_sales
+ 9.   -- write query to see top 5 customers based on total_sales
+	***sql
     select customer_id,
     sum(total_sale) as total
     from retail_sale
     group by 1
     order by 2 desc
     limit 5;
+***
+
     
-    -- write a query to create each shift and number of others( example <= morning,afternoon btw 12 and 17,evening >17)
+  10.  -- write a query to create each shift and number of others( example <= morning,afternoon btw 12 and 17,evening >17)
+	  ***sql
     with hourly
     as(
     select *,
@@ -93,11 +112,13 @@ group by 1,2;
     when hour(sale_time) between 12 and  17 then 'afternoon'
     else 'evening'
     end as shift
-    from retail_sale)
+    from retail_sale
+	)
     select shift,
     count(*) as total_orders
     from hourly
     group by shift
+*** 
     
     -- end of project
     
